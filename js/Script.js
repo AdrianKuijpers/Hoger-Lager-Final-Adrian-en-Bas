8aa3d0d1365c27f718f325413501f97e14df8991
AdrianKuijpers <302659507@student.rocmondriaan.nl>
2023-10-13 15:52:35 +0200

    Final Version Definitive
    Fixed old code

--- js/Script.js
+++ js/Script.js
@@ -2,9 +2,11 @@
 const button1 = document.querySelector(".Speler1");
 const owntext = document.querySelector(".owntext");
 let promptname = prompt("geef je naam!");
-if (promptname != null);
-owntext.textContent = promptname;
 
+if (promptname != null) {
+  owntext.textContent = promptname;
+}
+  
 //Result
 let results = {
   player1: 0,
@@ -15,11 +17,10 @@ let results = {
 function welcomeFunction() {
   btn3.disabled = true;
   btn4.disabled = true;
-  }
-
+}
 
 //dobbelsound Array
-const audioArray = ["Sound/dice-1.mp3", "Sound/dice-2.mp3", "Sound/dice-3.mp3"];
+const audioArray = ["sound/dice-1.mp3", "sound/dice-2.mp3", "sound/dice-3.mp3"];
 function playRandomAudio() {
   const audioIndex = Math.floor(Math.random() * audioArray.length);
   const audio = new Audio(audioArray[audioIndex]);
@@ -27,9 +28,9 @@ function playRandomAudio() {
 }
 
 //Result Sound
-let sound = new Audio("Sound/switch-sound.mp3");
-let Winsound = new Audio("Sound/yippee.mp3");
-let Losesound = new Audio("Sound/dark-souls-you-died.mp3");
+let sound = new Audio("sound/switch-sound.mp3");
+let Winsound = new Audio("sound/yippee.mp3");
+let Losesound = new Audio("sound/dark-souls-you-died.mp3");
 
 //Buttons to enable/disable
 const btn1 = document.querySelector(".btn1");
@@ -37,21 +38,23 @@ const btn3 = document.querySelector(".btn3");
 const btn4 = document.querySelector(".btn4");
 
 //dice with functions
-function throwdice(player) {
+let one1 = document.querySelector(".result1");
+let two2 = document.querySelector(".result2");
+function throwDice(player) {
   let result = Math.floor(Math.random() * 20) + 1;
-  const FirstDiceImage = "IMG/Number" + result + ".png";
-  const SecondDiceImage = "IMG/Number" + result + ".png";
+  const FirstDiceImage = "img/Number" + result + ".png";
+  const SecondDiceImage = "img/Number" + result + ".png";
   if (player === 1) {
     results.player1 = result;
     playRandomAudio();
-    document.getElementById("result1").innerHTML = "Speler 1: " + result;
+    one1 = "Speler 1: " + result;
     document.querySelectorAll("img")[0].setAttribute("src", FirstDiceImage);
     btn1.disabled = true;
     btn3.disabled = false;
     btn4.disabled = false;
   } else if (player === 2) {
     results.player2 = result;
-    document.getElementById("result2").innerHTML = "Speler 2: " + result;
+    two2 = "Speler 2: " + result;
     document.querySelectorAll("img")[1].setAttribute("src", SecondDiceImage);
     btn1.disabled = true;
     btn3.disabled = false;
@@ -63,11 +66,40 @@ function throwdice(player) {
 let counterDisplayElem = document.querySelector(".counter-display");
 let credit = 0;
 
+//Win and Lose alert
+function counterResultWin() {
+  if (credit === 10) {
+    setTimeout(function(){
+      location.reload();
+  }, 10000);
+  btn1.disabled = true;
+    Swal.fire({
+      title: "Je hebt Hoger/Lager gewonnen",
+      text: "het spel zal nu gereset worden zodat je nog een keer kan spelen",
+      icon: "success",
+    });
+    Winsound.play();
+    }
+  } 
+  function counterResultLose() {
+    if (credit === -5) {
+      setTimeout(function(){
+        location.reload();
+    }, 10000);
+    btn1.disabled = true;
+      Swal.fire({
+        title: "Je hebt Hoger/Lager verloren",
+        text: "het spel zal nu gereset worden zodat je nog een keer kan spelen",
+        icon: "error",
+      })
+      Losesound.play();
+      }
+    }
+
 //result of higher
-function higherresult() {
+function higherResult() {
   let higher = "";
   
-
   if (results.player1 > results.player2) {
     higher = Swal.fire({
       text: "Speler 1 heeft gewonnen!",
@@ -75,7 +107,7 @@ function higherresult() {
       timer: 3000,
       timerProgressBar: true,
       backdrop: `
-      rgba(0,100,0,0.3)
+        rgba(0,100,0,0.3)
       `
     });
     btn1.disabled = false;
@@ -83,9 +115,9 @@ function higherresult() {
     btn4.disabled = true;
     sound.play();
     credit++;
-    updatedisplay();
-    CounterResultWin();
-    function updatedisplay() {
+    updateDisplay();
+    counterResultWin();
+    function updateDisplay() {
       counterDisplayElem.innerHTML = credit;
     }
   } else if (results.player1 < results.player2) {
@@ -103,13 +135,13 @@ function higherresult() {
     btn4.disabled = true;
     sound.play();
     credit--;
-    updatedisplay();
-    CounterResultLose();
-    function updatedisplay() {
+    updateDisplay();
+    counterResultLose();
+    function updateDisplay() {
       counterDisplayElem.innerHTML = credit;
     }
   } else {
-    higher = Swal.fire({
+    Swal.fire({
       text: "Het is gelijkspel!",
       icon: "info",
       timer: 3000,
@@ -123,11 +155,10 @@ function higherresult() {
     btn4.disabled = true;
     sound.play();
   }
-  document.getElementById("hoger").innerHTML = higher;
 }
 
 //result when lower
-function lowerresult() {
+function lowerResult() {
   let lower = "";
 
   if (results.player1 < results.player2) {
@@ -145,9 +176,9 @@ function lowerresult() {
     btn4.disabled = true;
     sound.play();
     credit++;
-    updatedisplay();
-    CounterResultWin();
-    function updatedisplay() {
+    updateDisplay();
+    counterResultWin();
+    function updateDisplay() {
       counterDisplayElem.innerHTML = credit;
     }
   } else if (results.player1 > results.player2) {
@@ -165,9 +196,9 @@ function lowerresult() {
     btn4.disabled = true;
     sound.play();
     credit--;
-    updatedisplay();
-    CounterResultLose();
-    function updatedisplay() {
+    updateDisplay();
+    counterResultLose();
+    function updateDisplay() {
       counterDisplayElem.innerHTML = credit;
     }
   } else {
@@ -185,42 +216,10 @@ function lowerresult() {
     btn4.disabled = true;
     sound.play();
   }
-  document.getElementById("lager").innerHTML = lower;
 }
 
-//Win and Lose alert
-function CounterResultWin() {
-  if (credit === 10) {
-    setTimeout(function(){
-      location.reload();
-  }, 10000);
-  btn1.disabled = true;
-    Swal.fire({
-      title: "Je hebt Hoger/Lager gewonnen",
-      text: "het spel zal nu gereset worden zodat je nog een keer kan spelen",
-      icon: "success",
-    });
-    Winsound.play();
-    }
-  } 
-  function CounterResultLose() {
-    if (credit === -5) {
-      setTimeout(function(){
-        location.reload();
-    }, 10000);
-    btn1.disabled = true;
-      Swal.fire({
-        title: "Je hebt Hoger/Lager verloren",
-        text: "het spel zal nu gereset worden zodat je nog een keer kan spelen",
-        icon: "error",
-      })
-      Losesound.play();
-      }
-    }
-
-
 // The FPS counter for the META players, it checks how many frames you got.
-let fps = document.getElementById("fps");
+let fps = document.querySelector(".fps");
 let startTime = Date.now();
 let frame = 0;
 
@@ -234,129 +233,5 @@ function tick() {
   }
   window.requestAnimationFrame(tick);
 }
+
 tick();
-
-
-class Accordion {
-  constructor(el) {
-    // Store the <details> element
-    this.el = el;
-    
-    this.summary = el.querySelector("summary");
-    
-    this.content = el.querySelector(".content");
-
-    // Store the animation object (so we can cancel it if needed)
-    this.animation = null;
-    
-    this.isClosing = false;
-    
-    this.isExpanding = false;
-    
-    this.summary.addEventListener("click", (e) => this.onClick(e));
-  }
-
-  onClick(e) {
-    // Stop default behaviour from the browser
-    e.preventDefault();
-    
-    this.el.style.overflow = "hidden";
-    
-    if (this.isClosing || !this.el.open) {
-      this.open();
-      
-    } else if (this.isExpanding || this.el.open) {
-      this.shrink();
-    }
-  }
-
-  shrink() {
-    // Set the element as "being closed"
-    this.isClosing = true;
-
-    // Store the current height of the element
-    const startHeight = `${this.el.offsetHeight}px`;
-    
-    const endHeight = `${this.summary.offsetHeight}px`;
-
-    if (this.animation) {
-      
-      this.animation.cancel();
-    }
-
-    // Start a WAAPI animation
-    this.animation = this.el.animate(
-      {
-        
-        height: [startHeight, endHeight],
-      },
-      {
-        duration: 400,
-        easing: "ease-out",
-      }
-    );
-
-    // When the animation is complete, call onAnimationFinish()
-    this.animation.onfinish = () => this.onAnimationFinish(false);
-    // If the animation is cancelled, isClosing variable is set to false
-    this.animation.oncancel = () => (this.isClosing = false);
-  }
-
-  open() {
-    // Apply a fixed height on the element
-    this.el.style.height = `${this.el.offsetHeight}px`;
-    
-    this.el.open = true;
-    
-    window.requestAnimationFrame(() => this.expand());
-  }
-
-  expand() {
-    // Set the element as "being expanding"
-    this.isExpanding = true;
-    
-    const startHeight = `${this.el.offsetHeight}px`;
-    
-    const endHeight = `${
-      this.summary.offsetHeight + this.content.offsetHeight
-    }px`;
-
-    // If there is already an animation running
-    if (this.animation) {
-      
-      this.animation.cancel();
-    }
-
-    // Start a WAAPI animation
-    this.animation = this.el.animate(
-      {
-        
-        height: [startHeight, endHeight],
-      },
-      {
-        duration: 400,
-        easing: "ease-out",
-      }
-    );
-    
-    this.animation.onfinish = () => this.onAnimationFinish(true);
-    
-    this.animation.oncancel = () => (this.isExpanding = false);
-  }
-
-  onAnimationFinish(open) {
-    // Set the open attribute based on the parameter
-    this.el.open = open;
-    
-    this.animation = null;
-    
-    this.isClosing = false;
-    this.isExpanding = false;
-    
-    this.el.style.height = this.el.style.overflow = "";
-  }
-}
-
-document.querySelectorAll("details").forEach((el) => {
-  new Accordion(el);
-});
